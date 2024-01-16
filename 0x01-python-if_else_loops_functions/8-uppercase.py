370db842dd0cffea3193e2e2a4daafa38d4a6598
LanMalJR <lanmaljrlm>
2024-01-16 13:12:19 -0800

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+This script includes a function that
+outputs a string in uppercase.
+'''
+
+def uppercase(str):
+    newStr = ''
+
+    for c in str:
+        if ord(c) >= 65 and ord(c) <= 90:
+            newStr += c
+        elif ord(c) >= 97 and ord(c) <= 122:
+            charUpper = chr(ord(c) - 32)
+            newStr += charUpper
+        else:
+            newStr += c
+
+    print('{}'.format(newStr))
