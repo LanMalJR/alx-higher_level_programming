1e2df7d8a920dac9fe5d26782a3cafcdc1523ed2
LanMalJR <lanmaljrlm>
2024-01-16 13:08:47 -0800

    changes

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,7 +4,9 @@ This script features a function that verifies
 the presence of a lowercase character.
 '''
 
+
 def islower(c):
+
     if ord(c) <= 122 and ord(c) >= 97:
         return True
     else:
