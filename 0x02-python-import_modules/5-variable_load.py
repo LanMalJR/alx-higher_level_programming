669b404be9f054b7088e89f0dc77bfa1c961394d
LanMalJR <lanmaljrlm>
2024-01-18 11:00:44 -0800

    5-variable_load.py is added + its exec

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+This program brings in the variable 'a' from the file
+variable_load_5.py and then outputs its value.
+'''
+
+from variable_load_5 import a
+
+
+def main():
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
