e89e5221a5121c7afe533862fb75bf09a015a062
LanMalJR <lanmaljrlm>
2024-01-22 11:38:23 -0800

    l

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,13 +5,4 @@ Determines the largest integer in a list.
 
 
 def max_integer(my_list=[]):
-    largest = 0
-
-    if my_list:
-        for element in my_list:
-            if element > largest:
-                largest = element
-
-        return largest
-    else:
-        return None
+    return max(my_list, default=None)
