a9021ceceac21a55a5958a2a30ab9485c92f58bb
LanMalJR <lanmaljrlm@gmail.com>
2024-02-13 20:55:12 +0200

    added task 0 to 5 without the tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+This module includes a function named text_indentation()
+'''
+
+def text_indentation(text):
+    '''
+    Args:
+        text (str): Text to be processed
+
+    Raises:
+        TypeError: If text is not a string
+    '''
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    punctuation = ['.', '?', ':']
+    lastChar = ''
+
+    for currentChar in text:
+        if lastChar in punctuation and currentChar == ' ':
+            pass
+        else:
+            print(currentChar, end='')
+
+        if currentChar in punctuation:
+            print('\n')
+
+        lastChar = currentChar
