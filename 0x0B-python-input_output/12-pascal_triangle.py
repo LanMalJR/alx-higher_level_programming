b75a80f9af77df05cc67b1dcb9d8828edfbbf899
LanMalJR <lanmaljrlm@gmail.com>
2024-02-20 21:39:55 +0200

    updated task 7 and 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,22 +5,31 @@ Generate the Pascal triangle of n rows.
 
 
 def pascal_triangle(n):
-    pascal_triangle = []
-    previous = [1]
     if n <= 0:
-        return pascal_triangle
-    for row_number in range(n):
-        rowlist = []
-        if row_number == 0:
-            rowlist = [1]
-        else:
-            for i in range(row_number + 1):
-                if i == 0:
-                    rowlist.append(0 + previous[i])
-                elif i == (row_number):
-                    rowlist.append(previous[i - 1] + 0)
-                else:
-                    rowlist.append(previous[i - 1] + previous[i])
-        previous = rowlist
-        pascal_triangle.append(rowlist)
-    return pascal_triangle
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        current_row = [1]
+
+        for j in range(1, i):
+            current_row.append(triangle[i-1][j-1] + triangle[i-1][j])
+
+        current_row.append(1)
+
+        triangle.append(current_row)
+
+    return triangle
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,17 @@ Collects all arguments into a Python list,
 and subsequently stores them in a file
 '''
 
-from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+import sys
+fdumpJson = __import__('5-save_to_json_file').save_to_json_file
+loadJson = __import__('6-load_from_json_file').load_from_json_file
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+args = sys.argv[1:]
 
 try:
-    arglist = load_from_json_file("add_item.json")
+    pyData = loadJson('add_item.json')
 except FileNotFoundError:
-    arglist = []
+    pyData = []
 
-arglist += argv[1:]
-save_to_json_file(arglist, "add_item.json")
+pyData.extend(args)
+dumpJson(pyData, 'add_item.json')
