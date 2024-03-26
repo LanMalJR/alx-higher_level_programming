42ec65be61f8f9c7f2886088c1ccadd6b9eb35e0
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2024-03-26 22:39:28 +0200

    101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+This script showcases the average temperature (in Fahrenheit) for each city, arranged in descending order based on temperature.
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
