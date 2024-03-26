175ad76b6d7b45a367087704cc59bab7ab861a94
LanMalJR <lanmaljrlm>
2024-03-26 13:33:02 -0700

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
-This script calculates the average score of all records in the second_table.
+-- This script calculates the average score of all records in the second_table.
 SELECT AVG(score) as average
 FROM second_table;
