6e475190ff6674e2edc35d4550d3e66b91dbe6dc
LanMalJR <lanmaljrlm>
2024-04-22 11:29:31 -0700

    fix indednt and trailing white space task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,8 +3,8 @@
 function factorial (num) {
   if (Number.isNaN(num) || num === 0 || num === 1) {
     return 1;
-    } else {
-    let result = 1
+  } else {
+    let result = 1;
 
     for (let i = 2; i <= num; i++) {
       result *= i;
@@ -12,9 +12,9 @@ function factorial (num) {
     return result;
   }
 }
-  
+
 const args = process.argv;
 const num = parseInt(args[2], 10);
-  
+
 const result = factorial(num);
 console.log(result);
