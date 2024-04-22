32de156cbe1e4cab6ee6984440fdf61ac5986ce0
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2024-04-22 19:02:41 +0200

    Task 0 - 6

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const firstArg = args.splice(2, 1);
+
+if (typeof (firstArg[0]) === 'string') 
+{
+  console.log(firstArg[0]);
+} else 
+{
+  console.log('No argument');
+}
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const value = parseInt(args[2], 10);
+
+if (Number.isInteger(value)) 
+{
+  console.log('My number: ' + value);
+} else 
+{
+  console.log('Not a number');
+}
