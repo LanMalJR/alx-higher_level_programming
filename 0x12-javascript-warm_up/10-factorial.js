268da906e9e9f3ef92a5a3766f87f096e697a800
LanMalJR <lanmaljrlm>
2024-04-22 11:25:24 -0700

    fix indent & white space

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
 function factorial (num) {
-    if (Number.isNaN(num) || num === 0 || num === 1) {
-      return 1;
+  if (Number.isNaN(num) || num === 0 || num === 1) {
+    return 1;
     } else {
-      let result = 1;
-  
-      for (let i = 2; i <= num; i++) {
-        result *= i;
-      }
-      return result;
+    let result = 1
+
+    for (let i = 2; i <= num; i++) {
+      result *= i;
     }
+    return result;
   }
+}
   
-  const args = process.argv;
-  const num = parseInt(args[2], 10);
+const args = process.argv;
+const num = parseInt(args[2], 10);
   
-  const result = factorial(num);
-  console.log(result);
+const result = factorial(num);
+console.log(result);
