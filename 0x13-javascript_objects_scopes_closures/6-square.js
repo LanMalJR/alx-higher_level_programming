50045b4ae2e760ec75fa10ead48e8fa5277ea28f
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2024-04-23 19:17:53 +0200

    Task 6-10

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const parentSquare = require('./5-square.js');
+
+class Square extends parentSquare {
+  charPrint (c) {
+    let i = 0;
+    const char = (c) || 'X';
+
+    while (i < this.height) {
+      let j = 0;
+
+      while (j < this.width) {
+        process.stdout.write(char);
+        j++;
+      }
+      console.log();
+      i++;
+    }
+  }
+}
+
+module.exports = Square;
