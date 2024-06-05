3f36903233546c07fc4cb2b6deb06c226276807e
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2024-06-05 17:39:33 +0200

    Task 4-6 added

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const completedTasks = {};
+
+const fetchData = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    }
+    );
+  });
+};
+
+const fetchAndProcessData = async () => {
+  for (let i = 1; i <= 10; i++) {
+    const currentUrl = `${url}?userId=${i}`;
+    const resBody = await fetchData(currentUrl);
+    const completedArr = resBody.filter((item) => {
+      return item.completed === true;
+    });
+
+    completedTasks[`${i}`] = completedArr.length;
+  }
+
+  const result = JSON.stringify(completedTasks, null, 2).replace('{\n', '{').replace('\n}', ' }');
+  const formattedResult = result.replace('{  ', '{ ').replace(/"/g, "'");
+  console.log(formattedResult);
+};
+
+fetchAndProcessData();
